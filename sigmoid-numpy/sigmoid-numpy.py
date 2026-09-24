68d5755ee540f149ae96de05d12a30f7c05a7265
Rajeshwar Rao <rajeshwarrao055@gmail.com>
2026-09-24 09:41:37 +0530

    Solve sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -5,8 +5,4 @@ def sigmoid(x: list | float) -> np.ndarray | float:
     Returns the sigmoid value for a scalar or each element of a list.
     """
     # Write code here
-    if(isinstance(x, float) or isinstance(x, int)):
-        return 1/(1 + np.exp(-x))
-    else:
-        return [sigmoid(val) for val in x]
-    pass
+    return 1 / (1 + np.exp(-np.asarray(x)))
